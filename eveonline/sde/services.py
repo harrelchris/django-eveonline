f4d7037af063e0dcd2e0b77dadcd0130e1631df9
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-06-30 23:17:29 -0500

    Add foreign key enable/disable functions

--- eveonline/sde/services.py
+++ eveonline/sde/services.py
@@ -1,5 +1,21 @@
 import urllib.request
 
+from django.conf import settings
+from django.db import connection
+
+COMMANDS = {
+    "enable": {
+        "sqlite3": "PRAGMA foreign_keys = ON;",
+        "postgresql": "SET session_replication_role = 'origin';",
+        "mysql": "SET FOREIGN_KEY_CHECKS=1;",
+    },
+    "disable": {
+        "sqlite3": "PRAGMA foreign_keys = OFF;",
+        "postgresql": "SET session_replication_role = 'replica';",
+        "mysql": "SET FOREIGN_KEY_CHECKS=0;",
+    },
+}
+
 
 def get_remote_hash() -> str:
     request = urllib.request.Request(
@@ -8,3 +24,17 @@ def get_remote_hash() -> str:
     response = urllib.request.urlopen(request)
     content = response.read()
     return content.decode("utf-8").strip()
+
+
+def disable_foreign_key_verification():
+    engine = settings.DATABASES["default"]["ENGINE"].split(".")[-1]
+    with connection.cursor() as cursor:
+        command = COMMANDS["disable"][engine]
+        cursor.execute(command)
+
+
+def enable_foreign_key_verification():
+    engine = settings.DATABASES["default"]["ENGINE"].split(".")[-1]
+    with connection.cursor() as cursor:
+        command = COMMANDS["enable"][engine]
+        cursor.execute(command)
